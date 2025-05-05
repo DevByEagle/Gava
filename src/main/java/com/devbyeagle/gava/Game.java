a8d2b64ef7573dfbe46b5b3a4de0ba43fae816ee
DevByEagle <142937112+DevByEagle@users.noreply.github.com>
2025-05-05 01:32:38 +0000

    Added PC Keys and isKeyPressed in Game.java

--- src/main/java/com/devbyeagle/gava/Game.java
+++ src/main/java/com/devbyeagle/gava/Game.java
@@ -1,10 +1,29 @@
 package com.devbyeagle.gava;
 
 import javax.swing.JFrame;
+import java.awt.event.KeyEvent;
+import java.io.InputStream;
+import java.awt.*;
+import java.util.*;
 
 public class Game {
 	private JFrame window;
 	private boolean running = false;
+	private static final Set<Integer> keysPressed = new HashSet<>();
+	
+	static {
+		KeyboardFocusManager.getCurrentKeyboardFocusManager().addKeyEventDispatcher(new KeyEventDispatcher() {
+			public boolean dispatchKeyEvent(KeyEvent e) {
+				synchronized (InputStream.class) {
+					switch (e.getID()) {
+						case KeyEvent.KEY_PRESSED -> keysPressed.add(e.getKeyCode());
+						case KeyEvent.KEY_RELEASED -> keysPressed.remove(e.getKeyCode());
+					}
+				}
+				return false;
+			}
+		});
+	}
 	
 	public Game(int width, int height, String title) {
 		if (title == null || title == "") title = "Gava";
@@ -12,6 +31,10 @@ public class Game {
 		window = new JFrame(title);
 		window.setSize(width, height);
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+	
+	public boolean isKeyPreesed(int key) {
+		return keysPressed.contains(key);
 	}
 	
 	public void exit() {
--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -1,9 +1,19 @@
-//import com.devbyeagle.gava.*;
-import com.devbyeagle.gava.math.Vector2;
+import com.devbyeagle.gava.*;
+import com.devbyeagle.gava.input.*;
 
 public class Main {
 	public static void main(String[] args) {
-		final Game game = new Game();
-		game.run();		
+		final Game game = new Game(800, 600, null);
+		game.run();
+
+		while (true) {
+			System.out.println(game.isKeyPreesed(KeyCode.UP | KeyCode.W));
+
+			try {
+				Thread.sleep(5);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 }
